bcddfee7681e16deffbc2eee0e932318c2f672a9
Casamata_Patos <viniespezio21@gmail.com>
2026-02-25 17:32:58 -0300

    Add files via upload

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,17 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+// assets/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSuVZVVPbM8aDPgZ66QmqXvBhe8zP5sg",
+  apiKey: "AIzaSyDzdxKZqOgTjemtcRgM9arvGIYGcDdupGI",
   authDomain: "site-encomendas-patos.firebaseapp.com",
   projectId: "site-encomendas-patos",
-  storageBucket: "site-encomendas-patos.appspot.com",
-  messagingSenderId: "1144979472433",
-  appId: "1:1144979472433:web:234dbecc30a5a27d565510"
+  storageBucket: "site-encomendas-patos.firebasestorage.app",
+  messagingSenderId: "144979472433",
+  appId: "1:144979472433:web:234debcc30a5a27d56510",
+  measurementId: "G-9VGE5DSF6Z"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
